4f190d8ed7784ec0e128d9a5afe79c184a2be74c
ShantanuChougale <shantanuchougale@gmail.com>
2024-06-30 18:39:23 +0530

    Add files via upload

---
+++ task_1_portfolio/main.js
@@ -0,0 +1,28 @@
+const header = document.querySelector("header");
+
+window.addEventListener ("scroll", function(){
+    header.classList.toggle("sticky", window.scrollY > 120);
+});
+
+
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContent = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener("click", () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContent.forEach(tabContents => {
+            tabContents.classList.remove("skills_active")
+        })
+
+        target.classList.add("skills_active")
+
+
+        tabs.forEach(tab => {
+            tab.classList.remove("skills_active")
+        })
+
+        tab.classList.add("skills_active")
+    })
+})
